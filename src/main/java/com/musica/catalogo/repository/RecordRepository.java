5208862fc3b02baee99675389ada1198267509c7
Alfredo Pinkus <ampinkus@gmail.com>
2022-10-24 14:39:24 -0300

    Added class Records

---
+++ src/main/java/com/musica/catalogo/repository/RecordRepository.java
@@ -0,0 +1,16 @@
+package com.musica.catalogo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+* JpaRepository expose common database methods like find by Id, find all, delete, add.....
+* Spring @Repository annotation is used to indicate that the class provides the mechanism for storage, retrieval, search, update and delete operation on objects.
+* Spring @Repository Annotation:
+* Spring Repository annotation is a specialization of @Component annotation, so Spring Repository classes are autodetected by spring
+* framework through classpath scanning. Spring Repository is very close to DAO pattern where DAO classes are responsible for providing CRUD operations
+* on database tables. However, if you are using Spring Data for managing database operations, then you should use Spring Data Repository interface.
+* */
+@Repository
+public interface RecordRepository extends JpaRepository <Record, Long>{
+}
